0719b41d8b7a9e67b500a363f992ff7df4553668
Tuan Anh <yianh798@gmail.com>
2025-12-18 20:46:40 +0700

    Remove pem https

--- Frontend/Aurelia/vite.config.ts
+++ Frontend/Aurelia/vite.config.ts
@@ -1,15 +1,9 @@
 import { defineConfig } from "vite";
-import { readFileSync } from "node:fs";
+
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  server: {
-    https: {
-      key: readFileSync("C:/Windows/System32/localhost+2-key.pem"),
-      cert: readFileSync("C:/Windows/System32/localhost+2.pem"),
-    },
-  },
   build: {
     rollupOptions: {
       output: {
