13736e959f1455a3547f4774f24cf801707b0abf
Tuan Anh <yianh798@gmail.com>
2025-12-25 21:31:53 +0700

    Refac AI Measure

--- Frontend/Aurelia/src/Components/AiModelBodySize/Main.tsx
+++ Frontend/Aurelia/src/Components/AiModelBodySize/Main.tsx
@@ -131,8 +131,8 @@ export const Main: React.FC<MainCamera> = ({ isCameraOn, setIsCameraOn }) => {
         const rightHand = results.poseLandmarks?.[16];
         if (!rightHand) return;
 
-        const shoulderY = (shoulderLeft.y + shoulderRight.y) / 2;
-        const handInBox = rightHand.y < shoulderY;
+       const handInBox = rightHand.y < (shoulderLeft.y + shoulderRight.y) / 2 * 0.9;
+
 
         if (isCountingDownRef.current && !handInBox) {
           if (countdownIntervalRef.current !== null) {
